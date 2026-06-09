de275138c1c132fd0665a680798580818cfc8e6e
maykcaldas <mayk@futurehouse.org>
2026-06-09 01:18:12 -0700

    Added dependency to handle uuid generation?

--- rest-api/go.mod
+++ rest-api/go.mod
@@ -1,3 +1,5 @@
 module rest-api
 
 go 1.25.6
+
+require github.com/google/uuid v1.6.0 // indirect
--- rest-api/main.go
+++ rest-api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -10,11 +11,39 @@ type HealthResponse struct {
 	Status string `json:"status"`
 }
 
+type Book struct {
+	Id       string   `json:"id"`
+	Title    string   `json:"title"`
+	Author   string   `json:"author"`
+	Year     int      `json:"year"`
+	Category []string `json:"category"`
+}
+
+type BookList []Book
+
+var books BookList
+
 func main() {
 	router := http.NewServeMux()
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello world\n"))
+	})
+
+	router.HandleFunc("GET /books", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("All Books\n"))
+	})
+
+	router.HandleFunc("POST /books", func(w http.ResponseWriter, r *http.Request) {
+		// Stopping here. I need to implement this endpoint
+
+		w.Write([]byte("Create book\n"))
+	})
+
+	router.HandleFunc("GET /books/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+
+		w.Write([]byte(fmt.Sprintf("id: %s\n", id)))
 	})
 
 	router.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
